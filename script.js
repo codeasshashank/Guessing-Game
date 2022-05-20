f3c1861aa0f98be5f65d7e8d4ed57b5d21abe5f1
Shashank Pandey <85072168+codeasshashank@users.noreply.github.com>
2022-05-20 12:17:27 +0530

    color change

--- script.js
+++ script.js
@@ -9,10 +9,62 @@ function inputValue() {
     document.getElementById("guess").innerHTML = "Too High";
     document.getElementById("wrong").play();
     count = count - 1;
+    document.getElementById("bdy").style.backgroundColor = "#FF0000";
+    document.getElementById("first").style.backgroundColor = "#FF0000";
+    document.getElementById("second").style.backgroundColor = "#FF0000";
+    document.getElementById("third").style.backgroundColor = "#FF0000";
+    document.getElementById("firstone").style.backgroundColor = "#FF0000";
+    document.getElementById("secondone").style.backgroundColor = "#FF0000";
+    document.getElementById("secondsone").style.backgroundColor = "#FF0000";
+    document.getElementById("secondsone").style.color = "white";
+
+    document.getElementById("twoone").style.backgroundColor = "#FF0000";
+    document.getElementById("inpt").style.backgroundColor = "#FF0000";
+    document.getElementById("inpt").style.borderColor = "black";
+    document.getElementById("inpt").style.color = "white";
+
+    document.getElementById("guess").style.backgroundColor = "#FF0000";
+    document.getElementById("guess").style.color = "white";
+
+    document.getElementById("scr").style.backgroundColor = "#FF0000";
+    document.getElementById("scr").style.color = "white";
+
+    document.getElementById("btns").style.backgroundColor = "#FF0000";
+    document.getElementById("hs").style.backgroundColor = "#FF0000";
+    document.getElementById("hs").style.color = "white";
+
+    document.getElementById("firstsone").style.backgroundColor = "#FF0000";
+    document.getElementById("firstsone").style.color = "white";
   } else if (num < number) {
     document.getElementById("guess").innerHTML = "Too Low";
     document.getElementById("wrong").play();
     count = count - 1;
+    document.getElementById("bdy").style.backgroundColor = "#FF0000";
+    document.getElementById("first").style.backgroundColor = "#FF0000";
+    document.getElementById("second").style.backgroundColor = "#FF0000";
+    document.getElementById("third").style.backgroundColor = "#FF0000";
+    document.getElementById("firstone").style.backgroundColor = "#FF0000";
+    document.getElementById("secondone").style.backgroundColor = "#FF0000";
+    document.getElementById("secondsone").style.backgroundColor = "#FF0000";
+    document.getElementById("secondsone").style.color = "white";
+
+    document.getElementById("twoone").style.backgroundColor = "#FF0000";
+    document.getElementById("inpt").style.backgroundColor = "#FF0000";
+    document.getElementById("inpt").style.borderColor = "black";
+    document.getElementById("inpt").style.color = "white";
+
+    document.getElementById("guess").style.backgroundColor = "#FF0000";
+    document.getElementById("guess").style.color = "white";
+
+    document.getElementById("scr").style.backgroundColor = "#FF0000";
+    document.getElementById("scr").style.color = "white";
+
+    document.getElementById("btns").style.backgroundColor = "#FF0000";
+    document.getElementById("hs").style.backgroundColor = "#FF0000";
+    document.getElementById("hs").style.color = "white";
+
+    document.getElementById("firstsone").style.backgroundColor = "#FF0000";
+    document.getElementById("firstsone").style.color = "white";
   } else {
     document.getElementById("guess").innerHTML = "Number Matched";
     document.getElementById("bdy").style.backgroundColor = "#90EE90";
